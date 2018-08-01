845b61dbb73a4a78dc4a59c1ea3a6c2a2eb2a22c
Manasa Suresh <manasas@criticalmass.com>
2018-08-01 10:39:09 -0400

    Mock test

---
+++ test/firstTest.spec.js
@@ -0,0 +1,6 @@
+suite('Dealer locator component', function(){
+    test("Open delaer locator component and wait for it to load completely", function() {
+        browser.url('/')
+        browser.percySnapshot('Homepage');
+    })
+})
